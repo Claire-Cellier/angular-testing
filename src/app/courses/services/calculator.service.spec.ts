daf0b55c9dd3021f1697e85394b85938e0e1e1d2
Claire Cellier <ccellier@aubay.com>
2025-05-12 11:21:50 +0200

    test setup using beforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,27 +1,37 @@
 import { CalculatorService } from "./calculator.service";
-import { LoggerService } from "./logger.service";
 
 describe("CalculatorService", () => {
-    it("should add two numbers", () =>  {
-        //help to implement a jasmin spy
-        const logger = jasmine.createSpyObj('LoggerService', ["log"]);
 
-        //setupphase - prepapration the components or services that we can to test
-        const calculator = new CalculatorService(logger);
+    let calculator: CalculatorService,
+        loggerSpy: any;
+
+    //implement a jasmin spy
+    beforeEach(() => {
+        console.log("Calling beforeEach");
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+        calculator = new CalculatorService(loggerSpy);
+    })
+
+    it("should add two numbers", () => {
+        console.log("Add test");
+
+        //setup phase - prepapration the components or services that we can to test
+        // const calculator = new CalculatorService(logger);
 
         // execution phase - going to trigger the operation 
         const result = calculator.add(2, 2);
 
         // series of test assertions that are either going to fail or to be successful
         expect(result).toBe(4);
-        expect(logger.log).toHaveBeenCalledTimes(1)
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 
-    it("should substract two numbers", () =>  {
-        const calculator = new CalculatorService(new LoggerService());
+    it("should substract two numbers", () => {
+        console.log("Substract test");
 
         const result = calculator.subtract(2, 2);
 
         expect(result).toBe(0, "unexpected subtraction result");
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 })
