17b3dd4d37a1367b57303f624519bcb66773f2ef
Claire Cellier <ccellier@aubay.com>
2025-05-13 15:55:49 +0200

    Container Component Test Suite

--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
 
     advancedCourses$: Observable<Course[]>;
 
-    constructor(private coursesService: CoursesService) {
+    constructor(private readonly coursesService: CoursesService) {
 
     }
 
